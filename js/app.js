a42e04acea4db6493fdb38f70cc78de68ede9997
Blay199 <a.sorrentino03@gmail.com>
2022-08-04 00:14:01 +0200

    done

--- js/app.js
+++ js/app.js
@@ -1,46 +1,50 @@
 // creo la array di oggetti
 let team = [
     {
-        name : 'Waine Barret'
-        role : 'Founder & CEO'
+        name : 'Waine Barret',
+        role : 'Founder & CEO',
         Image : 'wayne-barnett-founder-ceo.jpg'
-    }
+    },
     {
-        name : 'Angela Caroll'
-        role : 'Chief Editor'
+        name : 'Angela Caroll',
+        role : 'Chief Editor',
         Image : 'angela-caroll-chief-editor.jpg'
-    }
+    },,
     {
-        name : 'Walter Gordon'
-        role : 'Office Manager'
+        name : 'Walter Gordon',
+        role : 'Office Manager',
         Image : 'walter-gordon-office-manager.jpg'
-    }
+    },
     {
-        name : 'Angela Lopez'
-        role : 'Social Media Manager'
+        name : 'Angela Lopez',
+        role : 'Social Media Manager',
         Image : 'angela-lopez-social-media-manager.jpg'
-    }
+    },
     {
-        name : 'Scott Estrada'
-        role : 'Developer'
+        name : 'Scott Estrada',
+        role : 'Developer',
         Image : 'scott-estrada-developer.jpg'
-    }
+    },
     {
-        name : 'Barbara Ramos'
-        role : 'Graphic Designer'
+        name : 'Barbara Ramos',
+        role : 'Graphic Designer',
         Image : 'barbara-ramos-graphic-designer.jpg'
     }
 ]
 
 console.log (team)
-// prendo il container 
 
-let container = document.querySelector(.container)
+// prendo il container 
+let container = document.querySelector('.container')
 
 // ciclo for each 
-    // recupero singoli attributi persone.nome
+team.forEach(element => {
+    console.log(element.name)
+    // recupero singoli attributi 
     // stampare attributi in console
-    // aggingo (+=) con innerhtml gli attributi nel container 
+    // aggingo (+=) con innerhtml gli attributi nel container
+    container.innerHTML += `<p> ${element.name} - ${element.role} - ${element.Image} </p>`
+});
     
 
 
