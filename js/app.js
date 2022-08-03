1e784a64d42551cff48cc76657fa0feb62306aff
Blay199 <a.sorrentino03@gmail.com>
2022-08-03 18:57:47 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,48 @@
+// creo la array di oggetti
+let team = [
+    {
+        name : 'Waine Barret'
+        role : 'Founder & CEO'
+        Image : 'wayne-barnett-founder-ceo.jpg'
+    }
+    {
+        name : 'Angela Caroll'
+        role : 'Chief Editor'
+        Image : 'angela-caroll-chief-editor.jpg'
+    }
+    {
+        name : 'Walter Gordon'
+        role : 'Office Manager'
+        Image : 'walter-gordon-office-manager.jpg'
+    }
+    {
+        name : 'Angela Lopez'
+        role : 'Social Media Manager'
+        Image : 'angela-lopez-social-media-manager.jpg'
+    }
+    {
+        name : 'Scott Estrada'
+        role : 'Developer'
+        Image : 'scott-estrada-developer.jpg'
+    }
+    {
+        name : 'Barbara Ramos'
+        role : 'Graphic Designer'
+        Image : 'barbara-ramos-graphic-designer.jpg'
+    }
+]
+
+console.log (team)
+// prendo il container 
+
+let container = document.querySelector(.container)
+
+// ciclo for each 
+    // recupero singoli attributi persone.nome
+    // stampare attributi in console
+    // aggingo (+=) con innerhtml gli attributi nel container 
+    
+
+
+
+    
